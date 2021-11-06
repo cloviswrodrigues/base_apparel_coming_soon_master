b7bce4a488bee541db3d8b9a0449a0274be8ed4b
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2021-11-05 23:55:29 -0300

    adicionando arquivos js e realizado ajuste do button

---
+++ frontend/scripts/scripts.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+const inputEmail = document.getElementById('email');
+
+form.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    validateInputs()
+})
+
+function validateInputs() {
+    
+}
